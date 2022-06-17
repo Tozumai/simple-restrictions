a9008ebd3c6f483c067ec76aef8451ca11ba7395
Tozumai <amerigog5@gmail.com>
2022-06-17 23:52:27 +0100

    Initial commit

---
+++ lua/autorun/server/simple-restrictions-server.lua
@@ -0,0 +1,31 @@
+util.AddNetworkString("RestrictionsNotify")
+
+local function RestrictionsTriggered( commandPlayer, commandText, entity )
+
+	local plyAllowed = Restrictions.Whitelist[commandPlayer:GetUserGroup()]
+
+	if ( not plyAllowed && GetConVar("RestrictionsWhitelist"):GetBool() == true ) then
+
+		Notification = "You cannot use "..commandText.." on "..entity:GetName()
+
+		net.Start("RestrictionsNotify")
+		net.WriteString(Notification)
+		net.Send(commandPlayer)
+
+		return false
+
+	else 
+
+		return true
+
+	end 
+
+end
+
+hook.Add("CanProperty", "RestrictPlayerProperty", function( ply, property, entity )
+
+	if ( GetConVar("RestrictionsEnabled"):GetBool() == false ) then return end
+
+	RestrictionsTriggered( ply, property, entity )
+
+end)
---
+++ lua/autorun/simple-restrictions-config.lua
@@ -0,0 +1,16 @@
+Restrictions = Restrictions or {}
+
+
+-- Turn on/off this addon, it essentiually won't run. 
+CreateConVar("RestrictionsEnabled", 1, FCVAR_ARCHIVE, "Enable or disable NoTarget", 0, 1)
+
+-- Turn on/off the allowed use only feature.
+CreateConVar("RestrictionsWhitelist", 0, FCVAR_ARCHIVE, "Enable or disable NoTargetWhitelist", 0, 1)
+
+-- Add the usergroup to the list, quite simple.
+Restrictions.Whitelist = {
+--  	["user"] = true,
+	["moderator"] = true,
+	["admin"] = true,
+	["superadmin"] = true,
+}
